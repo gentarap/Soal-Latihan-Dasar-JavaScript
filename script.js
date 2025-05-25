a4261ff9609eec160dd059c4c45108ed53684b26
Gentar <gentarasmara@gmail.com>
2025-05-25 23:48:29 +0700

    feat: add jawaban nomer 2

--- script.js
+++ script.js
@@ -1,17 +1,34 @@
 // Jawaban Nomer 1
 const keranjang = [
-    { nama: "Beras", harga: 12000, jumlah: 2 },
-    { nama: "Gula", harga: 10000, jumlah: 1 },
-    { nama: "Minyak", harga: 15000, jumlah: 3 },
-  ];
-  const hitungTotalBelanja = (keranjang) => {
-    let total = 0;
-    for (let i = 0; i < keranjang.length; i++) {
-      total += keranjang[i].harga * keranjang[i].jumlah;
-    }
-    return total;
-  };
-  
-  console.log(hitungTotalBelanja(keranjang));
+  { nama: "Beras", harga: 12000, jumlah: 2 },
+  { nama: "Gula", harga: 10000, jumlah: 1 },
+  { nama: "Minyak", harga: 15000, jumlah: 3 },
+];
+const hitungTotalBelanja = (keranjang) => {
+  let total = 0;
+  for (let i = 0; i < keranjang.length; i++) {
+    total += keranjang[i].harga * keranjang[i].jumlah;
+  }
+  return total;
+};
 
-  
+console.log(hitungTotalBelanja(keranjang));
+
+// Jawaban Nomer 2
+const produk = [
+  { nama: "Sabun", harga: 8000 },
+  { nama: "Sampo", harga: 15000 },
+  { nama: "Pasta Gigi", harga: 11000 },
+];
+
+const filterProdukMahal = (produk) => {
+  const hasil = [];
+  for (let i = 0; i < produk.length; i++) {
+    if (produk[i].harga > 10000) {
+      hasil.push(produk[i]);
+    }
+  }
+  return hasil;
+};
+
+console.log(filterProdukMahal(produk));
